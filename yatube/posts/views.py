10e3e71d83670b551eeb002eeaecbe7409641a13
a-glazyrin <glazyrin_simfer@mail.ru>
2022-12-07 16:02:06 +0300

    Отправка на ревью6697

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -85,7 +85,7 @@ def post_edit(request, post_id):
         return redirect('posts:post_detail', post_id)
     form = PostForm(request.POST or None,
         files=request.FILES or None,
-        instance=post)
+            instance=post)
     if form.is_valid():
         form.save()
         return redirect('posts:post_detail', post_id)
